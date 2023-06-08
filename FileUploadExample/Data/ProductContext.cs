1fb728e77d65e3fa0c41b37343935bef50504559
theZalmanian <thezalmanian@gmail.com>
2023-06-08 11:50:16 -0700

    Add project files.

---
+++ FileUploadExample/Data/ProductContext.cs
@@ -0,0 +1,14 @@
+﻿using FileUploadExample.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FileUploadExample.Data
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ FileUploadExample/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FileUploadExample.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductID { get; set; }
+
+        public string ProductTitle { get; set; }
+
+        public string ProductImageUrl { get; set; }
+    }
+}
